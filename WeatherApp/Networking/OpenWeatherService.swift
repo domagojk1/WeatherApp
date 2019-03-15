2acf6270fc683e572dd6352daaf8ee2c182c7498
DomagojKopic <kopic@uhp-digital.com>
2019-03-15 10:02:45 +0100

    Minor cleanup

--- WeatherApp/Networking/OpenWeatherService.swift
+++ WeatherApp/Networking/OpenWeatherService.swift
@@ -16,7 +16,6 @@ enum OpenWeatherService {
 
 extension OpenWeatherService: TargetType {
     typealias Method = Moya.Method
-
     static let apiKey = "95453dcae137c3505bb45e88ecc00272"
 
     var baseURL: URL {
@@ -55,6 +54,11 @@ extension OpenWeatherService: TargetType {
     }
 
     var sampleData: Data {
-        return "{\"coord\":{\"lon\":-0.13,\"lat\":51.51},\"weather\":[{\"id\":300,\"main\":\"Drizzle\",\"description\":\"light intensity drizzle\",\"icon\":\"09d\"}],\"base\":\"stations\",\"main\":{\"temp\":280.32,\"pressure\":1012,\"humidity\":81,\"temp_min\":279.15,\"temp_max\":281.15},\"visibility\":10000,\"wind\":{\"speed\":4.1,\"deg\":80},\"clouds\":{\"all\":90},\"dt\":1485789600,\"sys\":{\"type\":1,\"id\":5091,\"message\":0.0103,\"country\":\"GB\",\"sunrise\":1485762037,\"sunset\":1485794875},\"id\":2643743,\"name\":\"London\",\"cod\":200}".data(using: .utf8)!
+        switch self {
+        case .weather:
+            return SampleJson.weather.data(using: .utf8)!
+        case .forecast:
+            return SampleJson.forecast.data(using: .utf8)!
+        }
     }
 }
--- WeatherApp/Networking/RESTService.swift
+++
@@ -1,9 +0,0 @@
-//
-//  RESTService.swift
-//  WeatherApp
-//
-//  Created by UHP Mac 7 on 06/03/2019.
-//  Copyright © 2019 UHP. All rights reserved.
-//
-
-import Foundation
